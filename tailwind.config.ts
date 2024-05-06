9d06ad865b53cf78f4393a6d540a88dbde20c986
Adrian Bieniek <kontakt@abieniek.dev>
2024-05-06 15:10:02 +0200

    initial and config project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "#1F1F23",
+				content: "#FFFFFF",
+				primary: "#FFFFFF",
+				"primary-content": "#1F1F23",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
