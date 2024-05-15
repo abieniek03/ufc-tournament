3d60538f192c48957eecf523987eb85f2363d067
Adrian Bieniek <kontakt@abieniek.dev>
2024-05-15 16:18:44 +0200

    add: dialog mockup component

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import { CreateNewTournament } from "@/app/_components/dashboard/CreateNewTournament";
+
 export default function DashboardPage() {
   return (
     <>
-      <h1>Hello!</h1>
+      <div className="flex items-center justify-between">
+        <h1>Hello!</h1>
+        <CreateNewTournament />
+      </div>
     </>
   );
 }
---
+++ app/_components/dashboard/CreateNewTournament.tsx
@@ -0,0 +1,9 @@
+import { DialogMockup } from "../DialogMockup";
+
+export function CreateNewTournament() {
+  return (
+    <DialogMockup triggerLabel="Create new" title="Create new tournament">
+      <div>siema</div>
+    </DialogMockup>
+  );
+}
