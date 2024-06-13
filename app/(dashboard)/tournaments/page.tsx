2f54cd252c4d0db2da1e0851fece7d70ed2d8557
Adrian Bieniek <kontakt@abieniek.dev>
2024-06-13 15:45:33 +0200

    changes: all tournament page, add not found content

--- app/(dashboard)/tournaments/page.tsx
+++ app/(dashboard)/tournaments/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { Button } from "@/app/_components/Button";
 import { DashboardPageTitle } from "@/app/_components/dashboard/DashboardPageTitle";
 import { Tournament } from "@/app/_components/dashboard/Tournament";
@@ -12,19 +13,32 @@ export default async function TournamentsPage() {
     <>
       <div className="mb-8 flex items-center justify-between">
         <DashboardPageTitle>Your tournaments</DashboardPageTitle>
-        <Button styleType="primary" path="/create-tournament">
-          Create new
-        </Button>
-      </div>
-      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {userTournaments.data ? (
-          userTournaments.data.map((el: ITournament, index: number) => (
-            <Tournament key={index} data={el} />
-          ))
-        ) : (
-          <p className="text-center">No tournament found.</p>
+        {userTournaments.data && (
+          <Button styleType="primary" path="/create-tournament">
+            Create new
+          </Button>
         )}
       </div>
+
+      {userTournaments.data ? (
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {userTournaments.data.map((el: ITournament, index: number) => (
+            <Tournament key={index} data={el} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col items-center justify-center py-10">
+          <p className="mb-2 text-center text-2xl md:text-4xl lg:text-6xl">
+            No tournament found.
+          </p>
+          <Link
+            href="/create-tournament"
+            className="font-bold text-primary-500 hover:underline"
+          >
+            Create tournament
+          </Link>
+        </div>
+      )}
     </>
   );
 }
