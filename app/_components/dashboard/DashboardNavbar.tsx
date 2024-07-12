d962f8167bcc60497f39bc49c927708477c9cece
Adrian Bieniek <kontakt@abieniek.dev>
2024-07-12 19:58:20 +0200

    add: logo and favicon

--- app/_components/dashboard/DashboardNavbar.tsx
+++ app/_components/dashboard/DashboardNavbar.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
 
 export function DashboardNavbar() {
   return (
     <nav className="w-full border-b border-content/10">
-      <div className="mx-auto flex max-w-screen-lg items-center justify-between p-4">
-        <span className="uppercase">logo</span>
+      <div className="mx-auto flex max-w-screen-lg items-center justify-between px-4 py-2">
+        <Link href="/">
+          <Image src="/logo.svg" alt="" width={45} height={45} />
+        </Link>
         <UserButton afterSignOutUrl="/" />
       </div>
     </nav>
